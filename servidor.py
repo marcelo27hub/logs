d47540a25188ae23f1e5889f843bb34cde8426ac
Marcelo Valdez <marcelogvz27@gmail.com>
2025-11-05 01:47:14 -0300

    cree mi base de datos con un inicializador

--- servidor.py
+++ servidor.py
@@ -1,1 +1,31 @@
-print("servidor listo")
+#importmaos librerias que usaremos
+
+from flask import flask, request, jsonify
+from datetime import datetime, timezone
+import sqlite3
+
+app = flask(__name__)
+
+base_de_datos = "logs.db"
+
+#para inicializar nuestra base de datos
+def inicializar_db():
+    conexion = sqlite3.connect(base_de_datos)
+    cursor = conexion.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS logs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fecha_hora_evento TEXT,
+            servicio TEXT,
+            nivel TEXT, 
+            mensaje TEXT,
+            recibido_en TEXT 
+            
+        )
+     """)
+    conexion.commit()
+    conexion.close()
+    
+inicializar_db()
+
+    
