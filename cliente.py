a4723386e5d941b7e93b8d062f458ec5e866a5a9
Marcelo Valdez <marcelogvz27@gmail.com>
2025-11-06 16:55:00 -0300

    crre mis headers y una funcion de enviar log

--- cliente.py
+++ cliente.py
@@ -3,3 +3,24 @@ import requests
 import json
 from datetime import datetime, timezone
 import time
+
+servidor_url = "http://127.0.0.1:5000/logs"
+token = "TOKEN_SERVICIO_A" #cambia segun tu servicio
+
+HEADERS = {
+    "content-type" : "aplicacion/json",
+    "Autholrizacion" : f"Token {token}"
+}
+
+def enviar_log(mensaje, nivel= "INFO", servicio = "servicio-a"):
+    log= {
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "service": servicio,
+        "severity": nivel,
+        "message": mensaje
+
+    }
+    respuesta = requests.post(servidor_url, headers=HEADERS, data=json.dumps(log))
+
+    print("STATUS:", respuesta.status_code)
+    print("RESPUESTA:", respuesta.json())
